e44ed740645f458866c2982d03223208cae789ac
top-one-dev <toponedev@gmail.com>
2018-02-13 21:01:51 -0500

    edit some devise views...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,7 @@ Rails.application.configure do
   # remove if no need
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'tenderbooks.herokuapp.com' }
+  config.action_mailer.asset_host = 'https://tenderbooks.herokuapp.com'
 
   ActionMailer::Base.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
