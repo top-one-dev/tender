3ea32033efc852a6cf74da5e4b567ac81683a183
top-one-dev <toponedev@gmail.com>
2018-03-10 11:06:12 -0500

    update bid model

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,6 +1,24 @@
 class Bid < ApplicationRecord
+  include ActionView::Helpers::NumberHelper
+
   belongs_to :request
   belongs_to :supplier
   has_many :ianswers, dependent: :destroy
   has_many :qanswers, dependent: :destroy
+
+  def item_total
+  	item_total = 0
+  	self.ianswers.each do |ianswer|
+  		item_total += ianswer.unit_price * ianswer.item.quantity
+  	end
+  	item_total
+  end
+
+  def difference_budget
+  	bid_budget 		= Money.new(self.bid_budget*100, self.bid_currency)
+    expected_budget = Money.new(self.request.expected_budget*100, self.request.preferred_currency)
+    difference 		= bid_budget.exchange_to(self.request.preferred_currency) - expected_budget
+    percent 		= number_to_percentage( (difference/self.request.expected_budget) , precision: 2).to_s 
+    sprintf("%+.2f #{self.request.preferred_currency} <br>&nbsp;&nbsp;&nbsp;#{percent}", difference).html_safe
+  end
 end
