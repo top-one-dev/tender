4c59db2c71b39398ecb0040946edd1ec7a890fe9
top-one-dev <toponedev@gmail.com>
2018-03-14 11:24:05 -0400

    update company model

--- app/models/company.rb
+++ app/models/company.rb
@@ -13,4 +13,26 @@ class Company < ApplicationRecord
 		puts ActiveRecord::Base.connection.execute("select * from companies_users where user_id = #{user.id} and company_id = #{self.id}")
 		ActiveRecord::Base.connection.execute("select * from companies_users where user_id = #{user.id} and company_id = #{self.id}").first['created_at'].split('.').first
 	end
+
+	def suppliers
+		company_suppliers = []
+		Supplier.all.each do |supplier|
+			supplier.requests.each do |request|
+				if request.company == self
+					company_suppliers << supplier
+					unless supplier.user.nil?
+						company_suppliers.last[:name] = supplier.user.name
+						unless supplier.user.companies.exists?
+							company_suppliers.last[:company] = supplier.user.companies.first.name
+						end
+					end
+				end
+			end
+		end
+
+		unless company_suppliers.empty?
+			company_suppliers.uniq!
+		end
+		company_suppliers
+	end
 end
