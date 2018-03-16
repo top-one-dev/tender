1f9b75f0cad50356b76eb0a20e3282416224de6c
top-one-dev <toponedev@gmail.com>
2018-03-15 21:03:26 -0400

    update supplier...

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -3,4 +3,27 @@ class Supplier < ApplicationRecord
   has_and_belongs_to_many :requests
   has_many :messages, dependent: :destroy
   has_many :bids, dependent: :destroy
+
+  def company
+  	place_holder = Company.new(name: 'Unknown Company', introduction: 'No introduction...' )
+  	if self.user.nil?
+  		place_holder
+  	else
+  		if self.user.companies.exists?
+  			self.user.companies.first
+  		else
+  			place_holder
+  		end
+  	end  	
+  end
+
+  def correct_user
+  	place_holder = User.new(name: "No name", job_title: 'Unknown Job' )
+  	if self.user.nil?
+  		place_holder
+  	else
+  		self.user
+  	end	
+  end
+
 end
