93b1bfd71a1e32f8e5a21e84e3b13d1ec0ea035a
top-one-dev <toponedev@gmail.com>
2018-03-21 22:48:45 -0400

    update request form and helper

--- app/helpers/requests_helper.rb
+++ app/helpers/requests_helper.rb
@@ -1,2 +1,14 @@
 module RequestsHelper
+
+	def request_types
+		{
+			'RFQ' => 'Request for Quotation',
+			'RFI' => 'Request for Information',
+			'RFP' => 'Request for Proposal',
+			'EOI' => 'Expresssion of Interest',
+			'SPQ' => 'Supplier Pre-qualification',
+			'ITT' => 'Invitation to Tender'
+		}
+	end
+
 end
