bdf85c607706c462532a5e9614d86a3dd7ef44ce
top-one-dev <toponedev@gmail.com>
2018-03-21 23:17:37 -0400

    add new folder

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-folder = Folder.create([
+folders = [
 	{ name: 'Active Request', position: 1 }, 
 	{ name: 'Closed Request', position: 2 }, 
-	{ name: 'Archive Request', position: 3 }
-	])
+	{ name: 'Archived Request', position: 3 },
+	{ name: 'Undergoing Registration', position: 4}
+	]
+folders.each do |folder|
+	unless Folder.where(name: folder[:name]).exists?
+		Folder.create(folder)
+		puts "Created #{folder[:name]} folder successfully..."
+	else
+		puts "Some error to create #{folder[:name]} folder."
+	end
+end
