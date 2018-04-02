6fd9832d9a81e3663057f772068704cfdac34530
top-one-dev <toponedev@gmail.com>
2018-04-01 23:46:00 -0400

    create db for active record session

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180322192142) do
+ActiveRecord::Schema.define(version: 20180402024140) do
 
   create_table "bids", force: :cascade do |t|
     t.integer  "request_id"
@@ -269,6 +269,15 @@ ActiveRecord::Schema.define(version: 20180322192142) do
     t.datetime "updated_at",          null: false
     t.index ["company_id"], name: "index_requisitions_on_company_id"
     t.index ["user_id"], name: "index_requisitions_on_user_id"
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string   "session_id", null: false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
   create_table "stockholders", force: :cascade do |t|
