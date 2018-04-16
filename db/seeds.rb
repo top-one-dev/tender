11ee0f357c9a06937145de7001d33057b0aca729
top-one-dev <toponedev@gmail.com>
2018-04-16 11:31:12 -0400

    generate admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,3 +40,4 @@ categories.each do |category|
 		puts "#{category[:name]} category already exists..."
 	end
 end
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
