5580bc9a196ebe6a87321ded6b36e2e0f27033a0
top-one-dev <toponedev@gmail.com>
2018-08-24 11:02:07 -0400

    update request controller

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -581,8 +581,11 @@ class RequestsController < ApplicationController
   end
 
   def pdf
-    @request.to_pdf
-    send_file "#{Rails.root}/public/download/#{@request.created_at.strftime("%Y-%m-%d")}-#{@request.name}-##{@request.id}/#{@request.name}.pdf"
+    if @request.to_pdf
+      render json: { status: 'ok', url: "#{Rails.root}/public/download/#{@request.created_at.strftime("%Y-%m-%d")}-#{@request.name}-##{@request.id}/#{@request.name}.pdf" }
+    else
+      render json: { status: 'error' }
+    end
   end
 
   def download
